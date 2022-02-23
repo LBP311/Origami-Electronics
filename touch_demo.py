dbdcd26fe73c2246f33271542cbc6d8b2783c5c9
Luis Pizarro <lbp311@nyu.edu>
2022-02-23 05:05:49 +0000

    Attempt at creating a Touch Sensor Demo Stub Functions, see #27

---
+++ touch_demo.py
@@ -0,0 +1,10 @@
+import touch_sensor
+
+touch_sensor.setup()
+
+touch_sensor.Read_Pad0()
+touch_sensor.Read_Pad1()
+touch_sensor.Read_Pad2()
+touch_sensor.Read_Pad3()
+touch_sensor.Read_Pad4()
+touch_sensor.Read_All()
