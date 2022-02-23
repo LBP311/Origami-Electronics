47e7c56aa56780570d7234b7c2c18b2d3ff4563b
Luis Pizarro <lbp311@nyu.edu>
2022-02-23 06:02:54 +0000

    Added Loop to test touch_sensor.py ; Currently operational.

--- touch_demo.py
+++ touch_demo.py
@@ -1,4 +1,7 @@
 import touch_sensor
+import RPi.GPIO as GPIO
+import time
+import sys
 
 """
 Calls all functions within Touch Sensor Library.
@@ -6,9 +9,27 @@ Calls all functions within Touch Sensor Library.
 
 touch_sensor.setup()
 
-touch_sensor.Read_Pad0()
-touch_sensor.Read_Pad1()
-touch_sensor.Read_Pad2()
-touch_sensor.Read_Pad3()
-touch_sensor.Read_Pad4()
-touch_sensor.Read_All()
+
+try:
+	while True:
+		touch_sensor.Read_Pad0()
+		time.sleep(1)
+
+		touch_sensor.Read_Pad1()
+		time.sleep(1)
+
+		touch_sensor.Read_Pad2()
+		time.sleep(1)
+
+		touch_sensor.Read_Pad3()
+		time.sleep(1)
+
+		touch_sensor.Read_Pad4()
+		time.sleep(1)
+
+		touch_sensor.Read_All()
+		time.sleep(1)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	sys.exit()
