cc4ec7c9b0a3136ffbf6ec9deaf611ac3eef7ccb
Luis Pizarro <lbp311@nyu.edu>
2022-02-23 04:35:19 +0000

    Attempt at creating a Touch Sensor Setup Stub file, see #31. @mrm815 Please review and comment with suggestions.

---
+++ touch_setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='touch_sensor',
+	version='1.0',
+	description='5-Pad Capacitive Sensor Stub Library',
+	author='Luis Pizarro',
+	author_email='lbp311@nyu.edu',
+	url='https://github.com/LBP311/Origami-Electronics/',
+	py_modules=['touch_sensor'],
+	)
