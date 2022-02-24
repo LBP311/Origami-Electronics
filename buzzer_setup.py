5343851ae33c4c58ceb11dfd44a5c1b30affe2ac
Luis Pizarro <lbp311@nyu.edu>
2022-02-24 03:14:35 +0000

    Setup file for Piezo Buzzer Library

---
+++ buzzer_setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='buzzer',
+	version='1.0',
+	description='Small Enclosed Piezo w/ Wires',
+	author='Luis Pizarro',
+	author_email='lbp311@nyu.edu'
+	url='https://github.com/LBP311/Origami-Electronics/',
+	py_modules=['buzzer', 'RPi.GPIO', 'pigpio'],
+	)
