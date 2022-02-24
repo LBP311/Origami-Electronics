574b4f942c401b9896f60b2d273f1a1d4af2f73a
Luis Pizarro <lbp311@nyu.edu>
2022-02-24 04:41:40 +0000

    Liveness test for Buzzer

---
+++ buzzer.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import pigpio
+import sys
+
+pi = pigpio.pi()
+GPIO.setmode(GPIO.BCM)
+
+B_Pin = 13	# BCM 13 ; PWM 0
+
+pi.hardware_PWM(B_Pin, 5000, 500000)
+
+try:
+	while True:
+		continue
+except KeyboardInterrupt:
+	pi.hardware_PWM(B_Pin, 0, 0)
+	sys.exit()
