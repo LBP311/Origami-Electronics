2d6ff0992d5f162850ef326747bd5aa375e15bb2
Luis Pizarro <lbp311@nyu.edu>
2022-02-24 03:04:15 +0000

    Slight Correction of _py_modules_ list, see #27

--- touch_setup.py
+++ touch_setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='touch_sensor',
 	version='1.0',
-	description='5-Pad Capacitive Sensor Stub Library',
+	description='5-Pad Capacitive Sensor Library',
 	author='Luis Pizarro',
 	author_email='lbp311@nyu.edu',
 	url='https://github.com/LBP311/Origami-Electronics/',
-	py_modules=['touch_sensor'],
+	py_modules=['touch_sensor', 'RPi.GPIO'],
 	)
