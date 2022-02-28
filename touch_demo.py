db0c0fa6346634a9b89fb5c8c69c09c56a78f63e
Luis Pizarro <lbp311@nyu.edu>
2022-02-28 02:44:43 +0000

    Applied changes to be better test Sensor Library. #45

--- touch_demo.py
+++ touch_demo.py
@@ -1,4 +1,4 @@
-import touch_sensor
+from touch_sensor import *
 import RPi.GPIO as GPIO
 import time
 import sys
@@ -8,37 +8,76 @@ Calls all functions within Touch Sensor Library.
 touch_sensor.py
 """
 
-touch_sensor.setup()
-
-
+"""
 try:
-	touch_sensor.Wait_Pad(0)
-	touch_sensor.Wait_Pad(1)
-	touch_sensor.Wait_Pad(2)
-	touch_sensor.Wait_Pad(3)
-	touch_sensor.Wait_Pad(4)
 
-#	while True:
-#		continue
+	print("Setup")
+	setup()
 
-	"""
-		for i in range(6):
-			pad = int(input("Enter Pad \#:"))
+	print("\nRead_Pad")
+	# 10 Tests: 5 Pad Touches and 5 Pad with No Touch
+	for i in range(10):
+		pad = int(input("Enter Pad /#:"))
 
-			if (touch_sensor.Read_Pad(pad)):
-				print("Not Active")
-			else:
-				print("Active")
-			time.sleep(2)
+		if (Read_Pad(pad)):
+			print("Not Active")
+		else:
+			print("Active")
+		time.sleep(1)
 
-			print("Read-All")
-			active = touch_sensor.Read_All()
-			if (active is not None):
-				print("Active Pad: " + str(active))
-			else:
-				print("None Active")
-	"""
+	print("\nRead_All")
+	# 6 Tests: 5 Pad Touches and No Touch
+	for i in range(6):
+		active = Read_All()
+		if (active is not None):
+			print("Active Pad: " + str(active))
+		else:
+			print("None Active")
+		time.sleep(5)
+
+
+	print("\nWait_Pad")
+	for pad in range (5):
+		Wait_Pad(pad)
+
+	while True:
+		if GPIO.event_detected(PADS[0]):
+			print("Detected 0")
+		if GPIO.event_detected(PADS[1]):
+			print("Detected 1")
+		if GPIO.event_detected(PADS[2]):
+			print("Detected 2")
+		if GPIO.event_detected(PADS[3]):
+			print("Detected 3")
+		if GPIO.event_detected(PADS[4]):
+			print("Detected 4")
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
 	sys.exit()
+"""
+
+
+try:
+	print("Setup")
+	setup()
+
+	print("\nWait_All")
+	Wait_All()
+
+	while True:
+		if GPIO.event_detected(PADS[0]):
+			print("Detected 0")
+		if GPIO.event_detected(PADS[1]):
+			print("Detected 1")
+		if GPIO.event_detected(PADS[2]):
+			print("Detected 2")
+		if GPIO.event_detected(PADS[3]):
+			print("Detected 3")
+		if GPIO.event_detected(PADS[4]):
+			print("Detected 4")
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	sys.exit()
+
