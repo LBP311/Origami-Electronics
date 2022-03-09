767007e4bd148c10bc47377989b0ca3cc6e270de
LBP311 <98776496+LBP311@users.noreply.github.com>
2022-03-09 12:29:22 -0500

    Unified setup for overall library #18

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='origami',
+	version='1.0',
+	description='Origami Library',
+	author='Luis Pizarro',
+	author_email='lbp311@nyu.edu',
+	url='https://github.com/LBP311/Origami-Electronics/',
+	py_modules=['origami'],
+	)
