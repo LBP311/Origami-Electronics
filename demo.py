371435e2142d9f64cbb4788bce902656b27ea28f
LBP311 <98776496+LBP311@users.noreply.github.com>
2022-03-30 04:07:37 -0400

    Update on Previous demo.py
    Update includes the separate component-wise libraries in this single file, o_elec.py Library can correctly call all other functions correctly. 
    **Note:** Since Library was updated, this file will not run as intended. o_elec.py Library is still being worked on to be fully functional with every component correctly.

--- demo.py
+++ demo.py
@@ -1,1 +1,11 @@
-import origami
+import o_elec as oe
+
+oe.setup()
+oe.ReadPad(0)
+oe.ReadAll()
+oe.WaitPad(0)
+oe.WaitPadSequence([1])
+oe.PlaySong(1)
+oe.PlayNote(1, 2, 3)
+oe.LED_ON()
+oe.LED_OFF()
