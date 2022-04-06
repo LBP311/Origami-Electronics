71141e3d17a3cc3deeb5859695c3fba29f3cde05
LBP311 <98776496+LBP311@users.noreply.github.com>
2022-04-05 21:36:15 -0400

    Updated w/ Print Statements for each Lib. Function

--- demo.py
+++ demo.py
@@ -1,11 +1,15 @@
-import o_elec as oe
+import o_elec1 as oe
 
 oe.setup()
+
 oe.ReadPad(0)
 oe.ReadAll()
 oe.WaitPad(0)
-oe.WaitPadSequence([1])
-oe.PlaySong(1)
-oe.PlayNote(1, 2, 3)
+oe.WaitPadSequence(0)
+oe.PlaySong(0)
+oe.PlayNote(0,0,0)
 oe.LED_ON()
 oe.LED_OFF()
+oe.MotorVibLevel(4)
+oe.MotorOFF()
+oe.MotorON()
