bf0a049200be6f200970f51c4bf9f34949dde9d4
LBP311 <98776496+LBP311@users.noreply.github.com>
2022-05-08 20:47:41 -0400

    Final Update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='o_elec',
+setup(name='origami',
 	version='1.0',
 	description='Origami Electronics Library',
 	author='Luis Pizarro',
 	author_email='lbp311@nyu.edu',
 	url='https://github.com/LBP311/Origami-Electronics/',
-	py_modules=['o_elec'],
+	py_modules=['o_elec', 'Songs', 'Notes'],
 	)
 
